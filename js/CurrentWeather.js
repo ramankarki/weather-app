f1d9c8b04b332b17b01b0e72d227ca8d7bc31993
ramankarki <ramankarki40@gmail.com>
2020-12-02 17:53:12 +0545

    search for places section made slide when search for places button clicked

---
+++ js/CurrentWeather.js
@@ -0,0 +1,15 @@
+const searchForPlace = document.querySelector(".search-for-place-js");
+const searchForPlaceInput = document.querySelector(".SearchForPlacesInput-js");
+const currentWeather = document.querySelector(".CurrentWeather-js");
+const exitBtn = document.querySelector(".exit-icon");
+
+searchForPlaceInput.style.left = `-${currentWeather.clientWidth}px`;
+
+searchForPlace.addEventListener("click", () => {
+  searchForPlaceInput.style.left = 0;
+})
+
+exitBtn.addEventListener("click", () => {
+  searchForPlaceInput.style.left = `-${currentWeather.clientWidth}px`;
+});
+
