9888b8cf5446743522e13f67ba653583c433fb56
ramankarki <ramankarki40@gmail.com>
2020-12-04 23:23:19 +0545

    add location name of country if available

--- js/GetWeatherData.js
+++ js/GetWeatherData.js
@@ -18,7 +18,7 @@ async function getWeatherData(url, lat, long) {
   let cityData = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=114843591b3cb4652a2b94e65486c00a&units=metric&lang=en&lat=${lat}&lon=${long}`);
   cityData = await cityData.json();
   
-  tempC = Math.trunc(current.temp);
+  tempC = Math.round(current.temp);
   tempF = Math.round(current.temp * (9 / 5) + 32);
   if (activeTempUnit === "C") {
     setCurrentWeather(tempC, current.weather[0].description, cityData.name, cityData.sys.country);
@@ -52,8 +52,11 @@ function setCurrentWeather(temp, desc, cityName, country) {
   document.querySelector(".CurrentWeather-Value").textContent = temp;
   document.querySelector(".CurrentWeather-Unit").textContent = activeTempUnit;
   document.querySelector(".CurrentWeather-Desc").textContent = desc;
-  document.querySelector(".currentWeatherImg-js").setAttribute("src", `images/${desc}.png`);
-  document.querySelector(".Location-Address").textContent = cityName + ", " + country;
+  document.querySelector(".currentWeatherImg-js").setAttribute("src", `images/${desc}.svg`);
+  document.querySelector(".Location-Address").textContent = cityName;
+  if (country) {
+    document.querySelector(".Location-Address").textContent = cityName + ", " + country;
+  }
 }
 
 
@@ -73,16 +76,97 @@ function setFutureWeather(dailyData) {
 
   let elem = 0;
   for (let i = tomorrow; i <= 5; i++) {
-    maxElements[elem].textContent = Math.trunc(dailyData[i].temp.max);
+    maxElements[elem].textContent = Math.round(dailyData[i].temp.max);
     elem++;
   }
   elem = 0;
   for (let i = tomorrow; i <= 5; i++) {
-    minElements[elem].textContent = Math.trunc(dailyData[i].temp.min);
-    futureWeatherImg[elem].setAttribute("src", `images/${dailyData[i].weather[0].description}.png`);
+    minElements[elem].textContent = Math.round(dailyData[i].temp.min);
+    futureWeatherImg[elem].setAttribute("src", `images/${dailyData[i].weather[0].description}.svg`);
     futureWeatherDesc[elem].textContent = dailyData[i].weather[0].description;
     elem++;
   }
+}
+
+async function searchCity() {
+  let inputCity = document.querySelector(".Form-InputField").value;
+  document.querySelector(".Form-InputField").value = "";
+
+  let cityData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=114843591b3cb4652a2b94e65486c00a&units=metric&lang=en`);
+  cityData = await cityData.json();
+
+  if (cityData.cod !== "404") {
+    document.querySelector(".recent-cities-name").classList.add("recent-cities-name-visible");
+    addRecentCitySearch(inputCity);
+    delCitiesItem();
+    let urlNew = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityData.coord.lat}&lon=${cityData.coord.lon}&exclude=minutely,hourly,alerts&appid=114843591b3cb4652a2b94e65486c00a&units=metric&lang=en`;
+    getWeatherData(urlNew, cityData.coord.lat, cityData.coord.lon);
+    searchForPlaceInput.style.left = `-${currentWeather.clientWidth}px`;
+    document.querySelector(".errorInput").classList.remove("errorInput-visible");
+  } else {
+    document.querySelector(".errorInput").classList.add("errorInput-visible");
+  }
+}
+
+function addRecentCitySearch(inputValue) {
+  let citiesItem = document.createElement("div");
+  citiesItem.classList.add("Cities-Item");
+  let span = document.createElement("span");
+  span.classList.add("Cities-Name");
+  span.textContent = inputValue;
+  let img = document.createElement("img");
+  img.src = "images/right-angle-icon.svg";
+  img.alt = "right arrow icon";
+  img.classList.add("right-angle-icon");
+  citiesItem.appendChild(span);
+  citiesItem.appendChild(img);
+  document.querySelector(".Cities").prepend(citiesItem);
+  switchBackRecent();
+}
+
+function delCitiesItem () {
+  let cities = document.querySelectorAll(".Cities-Item");
+  if (cities.length > 5) {
+    cities[cities.length - 1].parentNode.removeChild(cities[cities.length - 1]);
+  }
+}
+
+// submit btn event listener
+document.querySelector(".Form-SubmitBtn").addEventListener("click", searchCity);
+
+// auto focus to input field
+document.querySelector(".search-for-place-js").addEventListener("click", () => {
+  document.querySelector(".Form-InputField").focus();
+});
+
+// keyboard event listener
+let inputField = document.querySelector(".Form-InputField");
+inputField.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" && inputField.value !== "") {
+    searchCity();
+  }
+});
+
+// short-cut key to open search input field
+document.addEventListener('keydown', function(event) {
+  if (event.altKey && event.key === 's') {
+    searchForPlaceInput.classList.add("SearchForPlacesInput-transition");
+    searchForPlaceInput.style.left = 0;
+    document.querySelector(".Form-InputField").focus();
+  }
+});
+
+
+// get data directly clicking previous searches
+function switchBackRecent() {
+  let cities = document.querySelectorAll(".Cities-Item");
+  cities.forEach(element => {
+    element.addEventListener("click", () => {
+      document.querySelector(".Form-InputField").value = element.textContent;
+      console.log(element.textContent);
+      searchCity();
+    });
+  });
 }
 
 
@@ -92,10 +176,6 @@ navigator.geolocation.getCurrentPosition((position) => {
   long = position.coords.longitude;
 
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&appid=114843591b3cb4652a2b94e65486c00a&units=metric&lang=en`;
-
-  // fetch()
-  //   .then((response) => response.json())
-  //   .then((json) => console.log(json));
 
   getWeatherData(url, lat, long);
 });
@@ -121,7 +201,7 @@ navigator.geolocation.getCurrentPosition((position) => {
   futureCardDates[0].textContent = "Tomorrow";
 })();
 
-
+// celcius button click event
 const futureUnits = document.querySelectorAll(".future-temp-unit-js");
 const futureValue = document.querySelectorAll(".future-temp-js");
 document.querySelector(".switchC").addEventListener("click", () => {
@@ -140,7 +220,7 @@ document.querySelector(".switchC").addEventListener("click", () => {
   });
 });
 
-
+// fahrenheit button click event
 document.querySelector(".switchF").addEventListener("click", () => {
   futureValue.forEach(element => {
     if (activeTempUnit === "C") {
